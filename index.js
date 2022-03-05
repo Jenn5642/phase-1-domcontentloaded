6e8f68297757ade1fd3221537438c82f37c2da90
Jenn Robbins <Jennrobbins3@gmail.com>
2022-03-05 18:57:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', ready);
+function ready(){
+    document.getElementById('text').innerHTML = "This is really cool!";
+    console.log('After DOM Loaded!')
+}
+
+
